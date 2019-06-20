0a160dfb2d8b31a2b290c65848eb133956e8eb4e
Zeke Hart <heirloch@gmail.com>
2019-06-20 08:43:17 -0400

    adds additional user functionality

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -58,6 +58,7 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
         ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
